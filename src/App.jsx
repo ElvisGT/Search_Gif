8b4a8a9aeee7d83a7df31e508086cf0574d10a19
ElvisGT <elvisgiltorres@gmail.com>
2022-06-08 17:06:24 -0400

    Configuracion incial lista

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+const App= () => {
+  return (
+    <div className="container">
+        <input className="search-bar" type="text" placeHolder="search Giphy" />
+  
+    </div>
+  );
+}
+
+export default App;
