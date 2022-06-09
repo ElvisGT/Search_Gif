910131ebb347569492c8fb951e9501cc18a5b7af
ElvisGT <elvisgiltorres@gmail.com>
2022-06-08 20:38:24 -0400

    Trabajando en los estilos de la vista principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import {Card} from './components/Card';
 
 const App= () => {
   return (
     <div className="container">
         <input className="search-bar" type="text" placeHolder="search Giphy" />
-  
+        <div className="container-cards">
+          <ul className='container-list'>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+            <li><Card /></li>
+
+          </ul>
+        </div>
+        
     </div>
   );
 }
